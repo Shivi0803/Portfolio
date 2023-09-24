86e8740c445631842463566fc0e9bac5b5f3ba17
Shivi0803 <shivi.shivani0803@gmail.com>
2023-09-24 20:21:50 +0530

    Portfolio Website

---
+++ process-index.php
@@ -0,0 +1,12 @@
+<?php
+
+$name = $_POST["name"];
+$email = $_POST["email"];
+$number = $_POST["mobile number"];
+$subject = $_POST["email subject"];
+$message = $_POST["message"];
+
+
+
+var_dump($name, $email , $number, $subject, $message );
+
